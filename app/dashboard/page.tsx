5f67a0d35c06f984bfaa7adde58a2b591f683e76
Richa <richapandey629@gmail.com>
2026-03-24 16:29:20 +0530

    added dashboard data and improved styling

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabaseClient';
+
+export default function Dashboard() {
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const { data, error } = await supabase
+      .from('contacts')
+      .select('*');
+
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
+
+
+    if (error) {
+      console.log(error);
+    } else {
+      setUsers(data);
+    }
+  };
+
+return (
+    
+  <div className="min-h-screen bg-gray-100 p-6">
+    <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
+
+    <div className="bg-white shadow rounded-lg overflow-hidden">
+      <table className="w-full text-left border-collapse">
+        
+
+        <thead className="bg-blue-900">
+          <tr>
+            <th className="p-3 border">Name</th>
+            <th className="p-3 border">Email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index} className="bg-blue-900">
+              <td className="p-3 border">{user.name}</td>
+              <td className="p-3 border">{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div>
+  </div>
+);
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -21,18 +21,24 @@ export default function Signup() {
   };
 
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className = "bg-white p-8 rounded-lg shadow-md w-80">
+      <h1 className="text-3xl font-semibold mb-5 text-center text-blue-950">Sign Up</h1>
       <input
+        className="w-full p-2 border rounded mb-3 text-black"
         placeholder="Email"
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
+        className="w-full p-2 border rounded mb-3 text-black"
         placeholder="Password"
         type="password"
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleSignup}>Sign Up</button>
+      <button 
+        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+        onClick={handleSignup}>Sign Up</button>
+    </div>
     </div>
   );
 }
