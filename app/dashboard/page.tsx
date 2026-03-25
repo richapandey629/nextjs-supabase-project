eaf31002ed00020c80308537fb3ba25a160f6312
Richa <richapandey629@gmail.com>
2026-03-25 17:46:37 +0530

    added logout button and rerouted to homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,10 +8,16 @@ export default function Dashboard() {
   const [users, setUsers] = useState<any[]>([]);
   const router = useRouter();
 
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.push('/');
+  };
+
   useEffect(() => {
+    checkUser();
     fetchUsers();
   }, []);
-
+ 
   const fetchUsers = async () => {
     const { data, error } = await supabase
       .from('contacts')
@@ -43,13 +49,17 @@ const checkUser = async () => {
 return (
     
   <div className="min-h-screen bg-gray-100 p-6">
-    <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
+    return (
+  <div className="min-h-screen bg-gray-100 p-6">
 
-    <div className="bg-white shadow rounded-lg overflow-hidden">
+    
+
+    <h1 className="text-3xl text-gray-900 text-center font-bold mb-6">Dashboard</h1>
+
+    <div className="bg-blue-900 shadow rounded-lg overflow-hidden">
       <table className="w-full text-left border-collapse">
-        
 
-        <thead className="bg-blue-900">
+        <thead className="bg-blue-900 text-white">
           <tr>
             <th className="p-3 border">Name</th>
             <th className="p-3 border">Email</th>
@@ -58,7 +68,7 @@ return (
 
         <tbody>
           {users.map((user, index) => (
-            <tr key={index} className="bg-blue-900">
+            <tr key={index} className="hover:bg-gray-100">
               <td className="p-3 border">{user.name}</td>
               <td className="p-3 border">{user.email}</td>
             </tr>
@@ -68,5 +78,15 @@ return (
       </table>
     </div>
   </div>
+  <button
+      className="mb-4 bg-red-700 text-white px-4 py-2 rounded "
+      onClick={handleLogout}
+    >
+      Logout
+    </button>
+  </div>
 );
 }
+
+
+   
