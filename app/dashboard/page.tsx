cb18305c89af76f4d815f00228a806ad44cb0aa6
Richa <richapandey629@gmail.com>
2026-03-26 12:54:39 +0530

    form added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,10 @@ import { useRouter } from 'next/navigation';
 
 export default function Dashboard() {
   const [users, setUsers] = useState<any[]>([]);
+  const [ email, setEmail ] = useState('');
+  const [ name , setName ] = useState('');
+  const [ phone , setPhone ] = useState('');
+  const [ age , setAge ] = useState('');
   const router = useRouter();
 
   const handleLogout = async () => {
@@ -46,17 +50,59 @@ const checkUser = async () => {
   }
 };
 
+const handleAddUser = async () => {
+  const { error } = await supabase.from('contacts').insert([
+    { name, email, phone, age }
+  ]);
+
+  if (error) {
+    alert(error.message);
+  } else {
+    alert("User Added Successfully");
+    fetchUsers();
+    setName('');
+    setEmail('');
+    setPhone('');
+    setAge('');
+  }
+}
+
 return (
     
   <div className="min-h-screen bg-gray-100 p-6">
-    return (
   <div className="min-h-screen bg-gray-100 p-6">
-
-    
 
     <h1 className="text-3xl text-gray-900 text-center font-bold mb-6">Dashboard</h1>
 
     <div className="bg-blue-900 shadow rounded-lg overflow-hidden">
+      <div className="mb-6 bg-white p-4 rounded shadow">
+        <h2 className = "text-lg font-bold mb-2 text-2xl text-blue-900 text-center">Add User</h2>
+
+        <input
+          className="border p-4 mb-2 text-blue-900"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}/>
+
+          <input
+          className="border p-4 mb-2 text-blue-900 block"
+          placeholder = "Email"
+          onChange= {(e) => setEmail(e.target.value)}/>
+
+          <input
+          className= "border p-4 mb-2 text-blue-900 block"
+          placeholder = "Phone"
+          onChange= {(e) => setPhone(e.target.value)}/>
+
+          <input
+          className = "border p-4 mb-2 text-blue-900"
+          placeholder = "Age"
+          onChange= {(e) => setAge(e.target.value)}/>
+
+          <button
+            className = "bg-blue-800 text-white px-4 py-2 rounded-2xl block"
+            onClick={handleAddUser}> Save </button>
+
+          </div>
       <table className="w-full text-left border-collapse">
 
         <thead className="bg-blue-900 text-white">
@@ -68,9 +114,9 @@ return (
 
         <tbody>
           {users.map((user, index) => (
-            <tr key={index} className="hover:bg-gray-100">
-              <td className="p-3 border">{user.name}</td>
-              <td className="p-3 border">{user.email}</td>
+            <tr key={index} className="hover:bg-blue-600">
+              <td className="p-3 border hover:bg-blue-600">{user.name}</td>
+              <td className="p-3 border hover:bg-blue-600">{user.email}</td>
             </tr>
           ))}
         </tbody>
@@ -87,6 +133,5 @@ return (
   </div>
 );
 }
-
 
    
